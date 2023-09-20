2867465e38097af398deadf7efd9501c804b8da4
khushi-gunnu17 <khushiastrogeek@gmail.com>
2023-09-21 05:06:07 +0530

    changes made

--- C/example.c
+++ C/example.c
@@ -1,17 +1,7 @@
-#include <stdio.h>
-
-void slice(char str[], int n, int m) {  // n and m are valid values
-    char newStr[100];
-    int j = 0;
-    for(int i = n; i<=m; i++, j++) {
-        newStr[j] = str[i]; 
-    }
-    newStr[j] = '\0';
-    puts(newStr);
-}
-
-int main() {
-    char str[100] = "Hello World";
-    slice(str, 3, 6);
+#include<stdio.h>
+int main()
+{
+    int x=printf("Hello");
+    printf("%d",x);
     return 0;
 }
